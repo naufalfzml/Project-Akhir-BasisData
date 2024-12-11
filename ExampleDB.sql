e2ff3ac5f7f30fadd071fe9025b9180e1e0057ad
Fawwaz Naufal M <naufalfzml07@gmail.com>
2024-12-11 21:20:54 +0700

    cuma nambahin contoh data di db

--- ExampleDB.sql
+++ ExampleDB.sql
@@ -1,63 +1,64 @@
-CREATE DATABASE EXAMPLE
+CREATE DATABASE Database_Hotel
 
-USE EXAMPLE
+USE Database_Hotel
 
-SELECT @@servername
+SELECT @@NAUFAL\SQLEXPRESS
 
--- Single
-CREATE TABLE TableA(
-    id INT IDENTITY,
-    name VARCHAR(32),
-)
-
--- 1 to 1
-CREATE TABLE TableBC (
-    id INT IDENTITY PRIMARY KEY,
-    name VARCHAR(32),
-    id_cb INT UNIQUE
+-CREATE TABLE KamarHotel (
+    id_kamar INT NOT NULL PRIMARY KEY IDENTITY(10001, 1),
+    tipe_kamar VARCHAR(50) NOT NULL,
+    harga_kamar DECIMAL(10, 2) NOT NULL,
+    nomor_kamar VARCHAR(10) UNIQUE NOT NULL,
+    status_kamar VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE TableCB (
-    id INT IDENTITY PRIMARY KEY,
-    name VARCHAR(32),
-    id_bc INT UNIQUE,
-    FOREIGN KEY (id_bc) REFERENCES TableBC(id)
+CREATE TABLE TabelKaryawan (
+    id_karyawan INT NOT NULL PRIMARY KEY IDENTITY(202201, 1),
+    nama_karyawan VARCHAR(100) NOT NULL,
+    jabatan VARCHAR(50) NOT NULL,
+    hp_karyawan VARCHAR(20) UNIQUE NOT NULL,
+    alamat_karyawan VARCHAR(100) NOT NULL,
+    gaji DECIMAL(10, 2) NOT NULL
 );
 
--- Add the foreign key for TableBC after TableCB is created
-ALTER TABLE TableBC
-ADD FOREIGN KEY (id_cb) REFERENCES TableCB(id);
-
--- 1 to many
-CREATE TABLE TableDE(
-    id INT IDENTITY PRIMARY KEY,
-    name VARCHAR(32)
+CREATE TABLE TamuHotel (
+    id_tamu INT NOT NULL PRIMARY KEY IDENTITY(30301, 1),
+    nama_tamu VARCHAR(100) NOT NULL,
+    jenis_kelamin VARCHAR(10) NOT NULL,
+    alamat_tamu VARCHAR(100) NOT NULL,
+    hp_tamu VARCHAR(20) UNIQUE NOT NULL,
+    tanggal_lahir DATE NOT NULL
 );
 
-CREATE TABLE TableEDMany(
-    id INT IDENTITY PRIMARY KEY,
-    name VARCHAR(32),
-    id_de INT,
-    FOREIGN KEY (id_de) REFERENCES TableDE(id)
+CREATE TABLE LayananTambahan (
+    id_service INT NOT NULL PRIMARY KEY IDENTITY(786001, 1),
+    id_karyawan INT NOT NULL,
+    nama_layanan VARCHAR(50) NOT NULL,
+    biaya_layanan DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (id_karyawan) REFERENCES TabelKaryawan(id_karyawan)
 );
 
--- many to many
-CREATE TABLE TableFGMany(
-    id INT IDENTITY PRIMARY KEY,
-    name VARCHAR(32)
+CREATE TABLE ReservasiKamar (
+    id_reservasi INT NOT NULL PRIMARY KEY IDENTITY(47401, 1),
+    id_service INT,
+    id_tamu INT NOT NULL,
+    id_kamar INT NOT NULL,
+    jumlah_layanan INT DEFAULT 0 CHECK (jumlah_layanan >= 0),
+    tanggal_check_in DATE NOT NULL,
+    tanggal_check_out DATE NOT NULL,
+    tanggal_reservasi DATE NOT NULL,
+    tanggal_pembayaran DATE NOT NULL,
+    metode_pembayaran VARCHAR(50) NOT NULL,
+    total_harga DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (id_service) REFERENCES LayananTambahan(id_service),
+    FOREIGN KEY (id_tamu) REFERENCES TamuHotel(id_tamu),
+    FOREIGN KEY (id_kamar) REFERENCES KamarHotel(id_kamar)
 );
 
-CREATE TABLE TableGFMany(
-    id INT IDENTITY PRIMARY KEY,
-    name VARCHAR(32)
+CREATE TABLE Reservasi_Layanan (
+    id_reservasi INT NOT NULL,
+    id_service INT NOT NULL,
+    PRIMARY KEY (id_reservasi, id_service),
+    FOREIGN KEY (id_reservasi) REFERENCES ReservasiKamar(id_reservasi),
+    FOREIGN KEY (id_service) REFERENCES LayananTambahan(id_service)
 );
-
-CREATE TABLE PivotFG(
-    id_fg INT, -- Foreign key to TableFGMany
-    id_gf INT, -- Foreign key to TableGFMany
-    PRIMARY KEY (id_fg, id_gf), -- Composite primary key to ensure uniqueness
-    FOREIGN KEY (id_fg) REFERENCES TableFGMany(id),
-    FOREIGN KEY (id_gf) REFERENCES TableGFMany(id)
-);
-
-SELECT * FROM TableBC
--- ExampleData.sql
+++ ExampleData.sql
@@ -1,61 +1,95 @@
-USE EXAMPLE
+USE Database_Hotel
 
--- Insert data into TableA
-INSERT INTO TableA (name)
-VALUES 
-('Name1'), ('Name2'), ('Name3'), ('Name4'), ('Name5'),
-('Name6'), ('Name7'), ('Name8'), ('Name9'), ('Name10'),
-('Name11'), ('Name12'), ('Name13'), ('Name14'), ('Name15'),
-('Name16'), ('Name17'), ('Name18'), ('Name19'), ('Name20'),
-('Name21'), ('Name22'), ('Name23'), ('Name24'), ('Name25'),
-('Name26'), ('Name27'), ('Name28'), ('Name29'), ('Name30');
+INSERT INTO KamarHotel (tipe_kamar, harga_kamar, nomor_kamar, status_kamar) VALUES
+('standard', 300000.00, 'A1', 'booked'),
+('deluxe', 750000.00, 'B1', 'booked'),
+('suite', 900000.00, 'C1', 'booked'),
+('standard', 300000.00, 'A2', 'booked'),
+('suite', 900000.00, 'C2', 'booked'),
+('deluxe', 750000.00, 'B2', 'booked'),
+('standard', 300000.00, 'A3', 'booked'),
+('suite', 900000.00, 'C3', 'booked'),
+('deluxe', 750000.00, 'B3', 'booked'),
+('suite', 900000.00, 'C4', 'booked'),
+('suite', 900000.00, 'C5', 'booked'),
+('deluxe', 750000.00, 'B4', 'booked'),
+('standard', 300000.00, 'A4', 'booked'),
+('standard', 300000.00, 'A5', 'booked'),
+('deluxe', 750000.00, 'B5', 'booked'),
+('meeting room', 1000000.00, 'D1', 'booked'),
+('deluxe', 750000.00, 'B6', 'booked'),
+('deluxe', 750000.00, 'B7', 'booked'),
+('deluxe', 750000.00, 'B8', 'booked'),
+('deluxe', 750000.00, 'B9', 'booked');
 
--- Insert data into TableBC and TableCB (1-to-1)
-INSERT INTO TableBC (name, id_cb) VALUES ('BCName1', NULL);
-INSERT INTO TableCB (name, id_bc) VALUES ('CBName1', SCOPE_IDENTITY());
-UPDATE TableBC SET id_cb = 1 WHERE id = 1
 
--- Insert data into TableDE (1-to-Many parent)
-INSERT INTO TableDE (name)
-VALUES 
-('DEName1'), ('DEName2'), ('DEName3'), ('DEName4'), ('DEName5'),
-('DEName6'), ('DEName7'), ('DEName8'), ('DEName9'), ('DEName10'),
-('DEName11'), ('DEName12'), ('DEName13'), ('DEName14'), ('DEName15'),
-('DEName16'), ('DEName17'), ('DEName18'), ('DEName19'), ('DEName20'),
-('DEName21'), ('DEName22'), ('DEName23'), ('DEName24'), ('DEName25'),
-('DEName26'), ('DEName27'), ('DEName28'), ('DEName29'), ('DEName30');
+INSERT INTO TabelKaryawan (nama_karyawan, jabatan, hp_karyawan, alamat_karyawan, gaji) VALUES
+('Jennifer Prima Sifabella', 'Resepsionis', '6285623148279', 'Jl. Dupak V', 4000000.00),
+('Jovanka Orlin Pradipa', 'Chef', '6287829183023', 'Jl. Hayam Wuruk No. 17', 5200000.00),
+('Laila Sasikirana Zahira', 'Chef', '6281000227332', 'Jl. Bhayangkara', 5200000.00),
+('Andriel Arkana', 'Chef', '6287200093000', 'Jl. Pisang Ambon', 5200000.00),
+('Brian Lukman Nugroho', 'Housekeeper', '6285289920019', 'Jl. Dringo No. 147', 3000000.00),
+('Fatin Colleen Delisia', 'Resepsionis', '6285772001264', 'Jl. Rusak No. 98', 4000000.00),
+('Gauri Anum Basimah', 'Housekeeper', '6287966111282', 'Jl. WR. Supratman III', 3000000.00),
+('Luthfi Oemar', 'Housekeeper', '6285100028393', 'Jl. Mpok Nori No. 8', 3000000.00),
+('Prataya Mahardika', 'Cleaning Service', '6287819000211', 'Jl. Agus Salim', 4600000.00),
+('Rafa Fauzan', 'Cleaning Service', '6285117729980', 'Jl. Slamet Riyadi', 4600000.00),
+('Cristiano Ronaldo', 'Housekeeper', '6282110095411', 'Jl. Ahmad Yani No. 3', 3000000.00),
+('Lionel Messi', 'Housekeeper', '6281677729905', 'Jl. Goyang', 3000000.00),
+('Kumis De Bruyne', 'Housekeeper', '6287816502124', 'Jl. Rejeki No. 7', 3000000.00),
+('Imam Surya', 'Cleaning Service', '6287111092222', 'Jl. Plaosan IV', 4600000.00),
+('Kaisar Nava', 'Cleaning Service', '6285100099922', 'Jl. Karonsih No. 15', 4600000.00);
 
--- Insert data into TableEDMany (1-to-Many child)
-DECLARE @parentId INT = 1;
-WHILE @parentId <= 10
-BEGIN
-    INSERT INTO TableEDMany (name, id_de)
-    VALUES 
-        (CONCAT('EDManyName', @parentId, '_1'), @parentId),
-        (CONCAT('EDManyName', @parentId, '_2'), @parentId),
-        (CONCAT('EDManyName', @parentId, '_3'), @parentId);
-    SET @parentId = @parentId + 1;
-END;
 
--- Insert data into TableFGMany (Many-to-Many)
-INSERT INTO TableFGMany (name)
-VALUES 
-('FGName1'), ('FGName2'), ('FGName3'), ('FGName4'), ('FGName5'),
-('FGName6'), ('FGName7'), ('FGName8'), ('FGName9'), ('FGName10'),
-('FGName11'), ('FGName12'), ('FGName13'), ('FGName14'), ('FGName15'),
-('FGName16'), ('FGName17'), ('FGName18'), ('FGName19'), ('FGName20'),
-('FGName21'), ('FGName22'), ('FGName23'), ('FGName24'), ('FGName25'),
-('FGName26'), ('FGName27'), ('FGName28'), ('FGName29'), ('FGName30');
+INSERT INTO TamuHotel (nama_tamu, jenis_kelamin, alamat_tamu, hp_tamu, tanggal_lahir) VALUES
+('Indra Kevin Filo', 'Laki-laki', 'Jl. Ahmad Yani', '6281234567890', '2003-11-13'),
+('Prajana Zayan', 'Laki-laki', 'Jl. Gubeng', '6281122334455', '2004-05-12'),
+('Alana Liora Gantari', 'Perempuan', 'Jl. Diponegoro', '6281398765432', '2005-09-12'),
+('Farhan Cahya', 'Laki-laki', 'Jl. Kalianak', '6281566778899', '1998-02-11'),
+('Sigit Ade Putra', 'Laki-laki', 'Jl. Sidorame', '6281723456789', '2001-04-13'),
+('Afira Rainey Keysa', 'Perempuan', 'Jl. Hayam Wuruk', '6281112233444', '2002-11-04'),
+('Naufal Daran', 'Laki-laki', 'Jl. Goyang', '6281955667788', '2000-12-09'),
+('Citra Liala Zanna', 'Perempuan', 'Jl. Kranggan', '6281911112222', '2002-12-03'),
+('Aisha Aileen Nathania', 'Perempuan', 'Jl. Sumberan', '6281333445566', '2001-07-23'),
+('Dimas Rafan', 'Laki-laki', 'Jl. Wonoyu', '6281376543210', '2001-09-04'),
+('Zinan Prayoga Aditama', 'Laki-laki', 'Jl. Deles', '6281599887766', '2005-10-13'),
+('Riordan Kevin', 'Laki-laki', 'Jl. Kalisari', '6281575443322', '2005-11-13'),
+('Dalila Sena Gawrita', 'Perempuan', 'Jl. Manyar', '6281678123456', '2003-03-01'),
+('Eliana Askana', 'Perempuan', 'Jl. Airlangga', '6281922334455', '2003-04-01'),
+('Indira Nuria', 'Perempuan', 'Jl. Keputran', '6281299887766', '1999-08-12'),
+('Idul Fitri', 'Perempuan', 'Jl Ir Sutami no 36, Jebres, Surakarta', '628123456789', '1994-04-21'),
+('Idul Adha', 'Laki-laki', 'Jl Ir Tentara pelajar no 36, Mojosongo, Surakarta', '6281133335555', '1990-10-28');
 
--- Insert data into TableGFMany (Many-to-Many)
-INSERT INTO TableGFMany (name)
-VALUES 
-('GFName1'), ('GFName2'), ('GFName3'), ('GFName4'), ('GFName5'),
-('GFName6'), ('GFName7'), ('GFName8'), ('GFName9'), ('GFName10'),
-('GFName11'), ('GFName12'), ('GFName13'), ('GFName14'), ('GFName15'),
-('GFName16'), ('GFName17'), ('GFName18'), ('GFName19'), ('GFName20'),
-('GFName21'), ('GFName22'), ('GFName23'), ('GFName24'), ('GFName25'),
-('GFName26'), ('GFName27'), ('GFName28'), ('GFName29'), ('GFName30');
+
+INSERT INTO LayananTambahan (id_karyawan, nama_layanan, biaya_layanan)
+VALUES
+    (202212, 'Extra Bed', 75000),
+    (202215, 'Room Service', 50000),
+    (202213, 'Extra Pillow', 30000),
+    (202202, 'Paket Makanan', 100000);
+
+INSERT INTO ReservasiKamar (id_service, id_tamu, id_kamar, jumlah_layanan, tanggal_check_in, tanggal_check_out, tanggal_reservasi, tanggal_pembayaran, metode_pembayaran, total_harga)
+VALUES
+(786001, 30301, 10001, 1, '2024-07-11', '2024-07-12', '2024-07-10', '2024-07-12', 'cash', 375000.00),
+(786003, 30302, 10002, 1, '2024-03-25', '2024-03-26', '2024-03-21', '2024-03-26', 'cash', 800000.00),
+(786001, 30303, 10003, 1, '2024-03-03', '2024-03-04', '2024-03-01', '2024-03-04', 'debit', 975000.00),
+(786002, 30304, 10004, 1, '2024-06-13', '2024-06-15', '2024-06-10', '2024-06-15', 'cash', 350000.00),
+(786004, 30305, 10005, 1, '2024-03-02', '2024-03-03', '2024-03-01', '2024-03-03', 'debit', 1000000.00),
+(786001, 30306, 10006, 1, '2024-04-13', '2024-04-14', '2024-04-12', '2024-04-14', 'debit', 825000.00),
+(786003, 30307, 10007, 1, '2024-04-07', '2024-04-08', '2024-04-05', '2024-04-08', 'debit', 330000.00),
+(786003, 30308, 10008, 1, '2024-06-29', '2024-06-30', '2024-06-27', '2024-06-30', 'debit', 930000.00),
+(786001, 30309, 10009, 1, '2024-05-13', '2024-05-15', '2024-05-11', '2024-05-15', 'cash', 825000.00),
+(786004, 30310, 10010, 1, '2024-05-30', '2024-05-31', '2024-05-27', '2024-05-31', 'debit', 1000000.00),
+(786004, 30311, 10011, 1, '2024-07-12', '2024-07-13', '2024-07-10', '2024-07-13', 'cash', 1000000.00),
+(786001, 30312, 10012, 1, '2024-04-09', '2024-04-11', '2024-03-29', '2024-04-11', 'debit', 825000.00),
+(786002, 30313, 10013, 1, '2024-04-01', '2024-04-02', '2024-02-29', '2024-04-02', 'cash', 350000.00),
+(786001, 30314, 10014, 1, '2024-06-08', '2024-06-09', '2024-06-06', '2024-06-09', 'debit', 375000.00),
+(786001, 30315, 10015, 1, '2024-05-01', '2024-05-03', '2024-04-29', '2024-05-03', 'debit', 825000.00),
+(786004, 30316, 10016, 25, '2024-05-20', '2024-05-21', '2024-05-03', '2024-05-21', 'cash', 8500000.00),
+(NULL, 30317, 10017, 0, '2024-06-05', '2024-06-06', '2024-05-30', '2024-06-06', 'cash', 750000.00),
+(NULL, 30317, 10018, 0, '2024-06-05', '2024-06-06', '2024-05-30', '2024-06-06', 'cash', 750000.00),
+(NULL, 30317, 10019, 0, '2024-06-05', '2024-06-06', '2024-05-30', '2024-06-06', 'cash', 750000.00),
+(NULL, 30317, 10020, 0, '2024-06-05', '2024-06-06', '2024-05-30', '2024-06-06', 'cash', 750000.00);
 
 -- Insert data into PivotFG (Many-to-Many relationship)
 DECLARE @fgId INT = 1, @gfId INT = 1;
