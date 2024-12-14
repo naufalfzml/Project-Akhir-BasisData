17e6c28eeddf16ebdd348fecd9063fd954e3b894
Fawwaz Naufal M <naufalfzml07@gmail.com>
2024-12-14 07:48:23 +0700

    logika create sudah selesai.

--- app/routes/routesReservasiKamar.py
+++ app/routes/routesReservasiKamar.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from connect import create_connection
-from datetime import date
+from datetime import date, datetime
 
 # Create a blueprint for modular routing
 routesReservasiKamar = Blueprint('routesReservasiKamar', __name__)
@@ -62,27 +62,78 @@ def create_ReservasiKamar():
         id_serviceReservasi = request.form['id_serviceReservasi']
         id_tamuReservasi = request.form['id_tamuReservasi']
         id_kamarReservasi = request.form['id_kamarReservasi']
-        jml_layananReservasi = request.form['jml_layananReservasi']
+        jml_layananReservasi = int(request.form['jml_layananReservasi'])
         tanggal_check_in = request.form['tgl_checkInReservasi']
         tanggal_check_out = request.form['tgl_checkOutReservasi']
         tanggal_pembayaran = request.form['tgl_bayarReservasi']
         metode_pembayaranReservasi = request.form['metode_bayarReservasi']
+
+        today_date = date.today().isoformat()
+        durasi_menginap = (datetime.strptime(tanggal_check_out, '%Y-%m-%d') - datetime.strptime(tanggal_check_in, '%Y-%m-%d')).days
         
+        #Ketika check-in dan check-out di hari yang sama, durasi menginap dihitung 1 hari
+        if durasi_menginap == 0 :
+            durasi_menginap = 1
+
+        #Validasi tanggal check-in tidak boleh lebih awal dari tanggal reservasi
+        if tanggal_check_in < today_date:
+            flash('Tanggal Check-In tidak boleh lebih awal dari tanggal reservasi!', 'danger')
+            return redirect(request.url)
+        
+        #Validasi tanggal pembayaran tidak boleh lebih awal dari tanggal reservasi
+        if tanggal_pembayaran < today_date:
+            flash('Tanggal Pembayaran tidak boleh lebih awal dari tanggal reservasi!', 'danger')
+            return redirect(request.url)
+        
+        # Validasi tanggal check-out tidak boleh lebih awal dari tanggal check-in
         if tanggal_check_out < tanggal_check_in :
             flash('Tanggal Check-Out tidak boleh lebih awal dari Tanggal Check-In. Silahkan input yang benar!', 'danger')
             return redirect(request.url)
-        # Hitung total_harga jika diperlukan (misal dari jumlah_layanan dan biaya layanan)
-        total_harga = 0 # Sesuaikan perhitungan Anda
 
         # Simpan data ke database
         conn = create_connection()
         if conn:
             cursor = conn.cursor()
             try:
+                cursor.execute('''
+                    SELECT COUNT(*) FROM ReservasiKamar 
+                    WHERE id_kamar = ? AND 
+                        (
+                            (tanggal_check_in <= ? AND tanggal_check_out >= ?) OR 
+                            (tanggal_check_in <= ? AND tanggal_check_out >= ?) OR
+                            (tanggal_check_in >= ? AND tanggal_check_out <= ?)
+                        )
+                ''', (id_kamarReservasi, tanggal_check_in, tanggal_check_in, tanggal_check_out, tanggal_check_out, tanggal_check_in, tanggal_check_out))
+                reservation_count = cursor.fetchone()[0]
+
+                if reservation_count > 0:
+                    flash('Kamar ini sudah dipesan untuk tanggal check-in dan check-out yang sama. Silakan pilih kamar lain.', 'danger')
+                    return redirect(request.url)
+                
+                #Menghitung biaya layanan tambahan berdasarkan id_service
+                cursor.execute('SELECT biaya_layanan FROM LayananTambahan WHERE id_service = ?', (id_serviceReservasi,))
+                harga_service = cursor.fetchone()
+
+                if not harga_service:
+                    flash('Service ID tidak valid!', 'danger')
+                    return redirect(request.url)
+                harga_service = harga_service[0]
+
+                #Menghitung harga kamar berdasarkan id_kamar
+                cursor.execute('SELECT harga_kamar FROM KamarHotel WHERE id_kamar = ?', (id_kamarReservasi,))
+                harga_kamar = cursor.fetchone()
+                if not harga_kamar:
+                    flash('Kamar ID tidak valid!', 'danger')
+                    return redirect(request.url)
+                harga_kamar = harga_kamar[0]
+
+                #Menghitung total harga reservasi kamar
+                total_harga = (harga_service * jml_layananReservasi) + (harga_kamar * durasi_menginap)
+
                 cursor.execute('''INSERT INTO ReservasiKamar
                                (id_service, id_tamu, id_kamar, jumlah_layanan, tanggal_check_in, tanggal_check_out, tanggal_reservasi, tanggal_pembayaran, metode_pembayaran, total_harga) 
                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
-                               (id_serviceReservasi, id_tamuReservasi, id_kamarReservasi, jml_layananReservasi, tanggal_check_in, tanggal_check_out, date.today(), tanggal_pembayaran, metode_pembayaranReservasi, total_harga))
+                               (id_serviceReservasi, id_tamuReservasi, id_kamarReservasi, jml_layananReservasi, tanggal_check_in, tanggal_check_out, today_date,  tanggal_pembayaran, metode_pembayaranReservasi, total_harga))
                 conn.commit()
                 flash('Reservasi Kamar added successfully!', 'success')
                 return redirect(url_for('routesReservasiKamar.ReservasiKamar'))
@@ -93,7 +144,6 @@ def create_ReservasiKamar():
                 conn.close()
         else:
             flash('Failed to connect to the database', 'danger')
-
 
     today_date = date.today()
 
@@ -123,8 +173,8 @@ def create_ReservasiKamar():
                            tamu_list=tamu_list, 
                            kamar_list=kamar_list)
 
-@routesReservasiKamar.route('/tableLayananTambahan/update/<id_service>', methods=['GET', 'POST'])
-def update_LayananTambahan(id_service):
+@routesReservasiKamar.route('/tableReservasiKamar/update/<id_reservasi>', methods=['GET', 'POST'])
+def update_ReservasiKamar(id_reservasi):
     conn = create_connection()
     if conn:
         cursor = conn.cursor()
@@ -145,17 +195,17 @@ def update_LayananTambahan(id_service):
                 conn.commit()
 
                 flash('Table A updated successfully!', 'success')
-                return redirect(url_for('routesKamarHotel.KamarHotel'))
+                return redirect(url_for('routesReservasiKamar.ReservasiKamar'))
 
             # For GET request, fetch current data to pre-fill the form
             cursor.execute('SELECT id_karyawan, nama_layanan, biaya_layanan FROM LayananTambahan WHERE id_service = ?', (id_service,))
             table = cursor.fetchone()
             if not table:
                 flash('Table not found!', 'danger')
-                return redirect(url_for('routesLayananTambahan.LayananTambahan'))
+                return redirect(url_for('routesReservasiKamar.ReservasiKamar'))
 
             # Pass the current data to the form
-            return render_template('editLayananTambahan.html', LayananTambahan={
+            return render_template('editReservasiKamar.html', ReservasiKamar={
                 'id_karyawan'    : table[0],
                 'nama_layanan'   : table[1],
                 'biaya_layanan'      : table[2]
@@ -167,10 +217,11 @@ def update_LayananTambahan(id_service):
             conn.close()
     else:
         flash('Error: Unable to connect to the database.', 'danger')
-        return redirect(url_for('routesLayananTambahan.continents'))
+        return redirect(url_for('routesReservasiKamar.continents'))
 
-@routesReservasiKamar.route('/tableLayananTambahan/delete/<id_service>', methods=['POST'])
-def delete_continent(id_service):
+
+@routesReservasiKamar.route('/tableReservasiKamar/delete/<id_reservasi>', methods=['POST'])
+def delete_continent(id_reservasi):
     # Get a connection to the database
     conn = create_connection()
     
@@ -179,11 +230,11 @@ def delete_continent(id_service):
         cursor = conn.cursor()
         try:
             # Delete the tableA from the database
-            cursor.execute('DELETE FROM LayananTambahan WHERE id_service = ?', (id_service,))
+            cursor.execute('DELETE FROM ReservasiKamar WHERE id_reservasi = ?', (id_reservasi,))
             conn.commit()  # Commit the transaction
             
             # Redirect to the tableA list with a success message
-            flash('Table LayananTambahan deleted successfully!', 'success')
+            flash('Table ReservasiKamar deleted successfully!', 'success')
         except Exception as e:
             flash(f'Error: {str(e)}', 'danger')
         finally:
@@ -192,4 +243,4 @@ def delete_continent(id_service):
     else:
         flash('Error: Unable to connect to the database.', 'danger')
     
-    return redirect(url_for('routesLayananTambahan.LayananTambahan'))
+    return redirect(url_for('routesReservasiKamar.ReservasiKamar'))
